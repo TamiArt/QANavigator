d0a7dbe94710ec35b81fe4f9b69f0edf1f4e3cef
ArTami <kitana52rus@gmail.com>
2026-08-18 12:24:52 +0300

    Добавить промежуточные числовые границы

--- src/app/features/beginner-wizard/types.ts
+++ src/app/features/beginner-wizard/types.ts
@@ -19,6 +19,7 @@ export interface FieldConfig {
   required: boolean;
   min?: number;
   max?: number;
+  intermediateBoundaries?: number[];
   minLength?: number;
   maxLength?: number;
   format?: string;
